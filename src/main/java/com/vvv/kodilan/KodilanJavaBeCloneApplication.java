5d691bdb66a68f268791f94ebafd7f004db08334
volkanalb <volkanalb@gmail.com>
2020-07-31 06:42:54 +0300

    Initial commit

---
+++ src/main/java/com/vvv/kodilan/KodilanJavaBeCloneApplication.java
@@ -0,0 +1,13 @@
+package com.vvv.kodilan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodilanJavaBeCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodilanJavaBeCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vvv/kodilan/KodilanJavaBeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvv.kodilan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodilanJavaBeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
